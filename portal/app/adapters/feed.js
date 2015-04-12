4b426164f39b9e8103c03e88198e0cc27493de10
Tam Pham <tampham47@live.com>
2015-04-12 21:25:55 +0700

    understand ember adapter, it's very cool

--- portal/app/adapters/feed.js
+++ portal/app/adapters/feed.js
@@ -1,7 +1,12 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
+  url: 'localhost:3000',
   getAll: function() {
-    alert('Feed Adapter getAll');
+    console.log('Feed Adapter getAll');
+    return null;
+  },
+  get: function() {
+    return this.ajax(url, 'GET', {})
   }
 });
--- portal/app/controllers/feed/index.js
+++ portal/app/controllers/feed/index.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  model: function() {
+    this.feedAdapter.getAll();
+    return null;
+  },
   actions: {
     navToDetail: function () {
+      this.container.lookup('adapter:feed').getAll();
       this.transitionToRoute('feed.detail');
     }
   }
