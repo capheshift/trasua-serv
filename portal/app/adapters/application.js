1e7ab83d2cc072c05f8d9f2e5e0c78ac43ec8c7b
Tam Pham <tampham47@live.com>
2015-04-12 23:10:26 +0700

    define list of apis

--- portal/app/adapters/application.js
+++ portal/app/adapters/application.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
+  url: 'localhost:3001/'
 });
--- portal/app/adapters/feed.js
+++ portal/app/adapters/feed.js
@@ -1,12 +1,23 @@
 import ApplicationAdapter from './application';
 
-export default ApplicationAdapter.extend({
-  url: 'localhost:3000',
-  getAll: function() {
-    console.log('Feed Adapter getAll');
-    return null;
-  },
-  get: function() {
-    return this.ajax(url, 'GET', {})
+var apiList, result = {};
+
+// define list of api
+apiList = [
+  { path: 'getByLocation', method: 'GET'},
+  { path: 'getAll', method: 'GET'},
+  { path: 'create', method: 'POST'}];
+
+// create functions with each api link
+$.each(apiList, function(index, item) {
+  result[item.path] = function(params) {
+    // in this case, 'this' is a object was referenced from Adapter
+    console.log(this.url + item.path);
+    return this.ajax(this.url + item.path, item.method, params || {});
   }
 });
+
+// export default ApplicationAdapter.extend({
+// });
+
+export default ApplicationAdapter.extend(result);
