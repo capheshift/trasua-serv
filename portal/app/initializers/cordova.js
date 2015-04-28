aae979f9a7edb832a400b125773d03d87d4df98a
Tam Pham <tampham47@live.com>
2015-04-28 20:18:19 +0700

    stable version 0.9.0

--- portal/app/initializers/cordova.js
+++ portal/app/initializers/cordova.js
@@ -2,18 +2,14 @@
 
 export function initialize(container, app) {
   if (window.cordova) {
-    console.log('defering app launch until cordova is ready...');
-    // alert('defering app launch until cordova is ready...');
+    // console.log('defering app launch until cordova is ready...');
     app.deferReadiness();
 
     document.addEventListener('deviceready', function() {
-      console.log('lift off!');
-      // alert('deviceready');
       app.advanceReadiness();
     }, false);
   } else {
-    console.log('ember app is runing on browser');
-    // alert('ember app is runing on browser');
+    // console.log('ember app is runing on browser');
   }
 }
 
