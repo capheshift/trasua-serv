ca3fcbf286f0b22d40906a6355422d5d0dfa901b
Tam Pham <tampham47@live.com>
2015-05-28 23:42:16 +0700

    rebase

--- portal/app/controllers/feed/detail.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  actions: {
-    goBack: function(){
-      this.transitionToRoute('feed.index');
-    }
-  }
-});
--- react-ui/app/main.js
+++
@@ -1,6 +0,0 @@
-/** @jsx React.DOM */
-
-var React = require('react');
-var APP = require('./views/app');
-
-React.renderComponent(<APP/>, document.getElementById('main'));
